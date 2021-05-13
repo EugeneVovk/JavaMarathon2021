5c98354faf55e8d32f1b8fca1e524c41c512607b
Eugene Vovk <eugenevovk@gmail.com>
2021-05-13 09:52:16 +0300

    add day9

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -3,5 +3,13 @@ package day9.Task1;
 public class Task1 {
     public static void main(String[] args) {
 
+        Student student = new Student("Petrov", "Fifth stream");
+        Teacher teacher = new Teacher("Marya Ivanovna", "To learn Java");
+
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+
+        student.printInfo();
+        teacher.printInfo();
     }
 }
