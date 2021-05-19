e5c5ecd008983295a90788d54f136568e5fc1f72
Eugene Vovk <eugenevovk@gmail.com>
2021-05-19 16:13:37 +0300

    add day14 with Neil's comments and tasks

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,53 @@
 package day14;
 
+/**
+ * Создать .txt файл в папке проекта, как показано в видео (урок 36, время 15:30).
+ * Заполнить его вручную десятью произвольными числами.
+ * Реализовать статический метод printSumDigits(File file),
+ * который считает сумму всех чисел в этом файле и выводит ее на экран.
+ * Если файла не существует в папке проекта, в программе необходимо выбрасывать исключение
+ * и выводить в консоль сообщение “Файл не найден”. Помимо этого,
+ * если чисел в файле меньше или больше 10, необходимо выбрасывать исключение
+ * и выводить в консоль сообщение “Некорректный входной файл”.
+ * Вызвать данный метод в методе main().
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("text");
 
+        printSumDigits(file);
+
+    }
+
+    public static void printSumDigits(File file) {
+
+        try {
+            Scanner scanner = new Scanner(file);
+            //  если файл найден, значит мы идём дальше и считываем следующую строку, записывая её в переменную
+            String line = scanner.nextLine();
+            //  делим строку по пробелам и кладём её в массив
+            String[] numbers = line.split(" ");
+            //  проверяем, сколько чисел в файле
+            if (numbers.length != 10) {
+                //  выбрасываем исключение
+                throw new IllegalArgumentException();
+            }
+            //  если всё верно идём дальше и считаем сумму всех чисел в файле
+            int sum = 0;
+            //  переводим строки в цифры
+            for (String number : numbers)
+                sum += Integer.parseInt(number);
+            System.out.println(sum);
+            //  Ловим все выброшенные исключения
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,56 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Создать класс “Человек” с полями “имя” и “возраст”.
+ * Реализовать статический метод List<Person> parseFileToObjList(File file),
+ * который считывает содержимое того же файла people.txt,
+ * создает экземпляры класса “Человек” и возвращает список объектов.
+ * Получить данный список в методе main() и распечатать его в консоль.
+ * В случае, если файла не существует в папке проекта, в программе необходимо выбрасывать исключение
+ * и выводить в консоль сообщение “Файл не найден”. Помимо этого, если значение возраста отрицательно,
+ * необходимо выбрасывать исключение и выводить в консоль сообщение “Некорректный входной файл”.
+ * <p>
+ * Пример ответа: [{name='Mike', year=24}, {name='John', year=19}, {name='Anna', year=20}, {name='Miguel', year=5}]
+ */
+
 public class Task3 {
     public static void main(String[] args) {
+        File file = new File("people");
 
+        System.out.println(parseFileToObjList(file));
+
+    }
+
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            //  читаем строки файла
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] person = line.split(" ");
+
+                if (Integer.parseInt(person[1]) < 0)
+                    throw new IllegalArgumentException();
+
+                //  создаём объект и в параметрах имя и возраст (переводим в целое число)
+                Person current = new Person(person[0], Integer.parseInt(person[1]));
+                people.add(current);
+            }
+            return people;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
     }
 }
